58a7459a87b545c1b08836929fb2277428622961
K0N0R <218296@student.pwr.edu.pl>
2015-05-15 22:54:42 +0200

    Server moved out

--- Server/app.ts
+++
@@ -1,7 +0,0 @@
-﻿import express = require("express");
-import http = require("http");
-var serveStatic = require('serve-static');
-var app = express();
-var server = app.listen(80);
-
-app.use(serveStatic("./static", { index: ["index.html"] }));
